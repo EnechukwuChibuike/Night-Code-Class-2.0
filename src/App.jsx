a131f95d2ca398b685d298809d0d3c3b1565131c
chibuike <enechukwuchibuike0@gmail.com>
2024-11-06 22:11:00 +0100

    added state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import About from "./About";
 import "./App.css";
-import Header from "./Header";
-import Hero from "./Hero";
 
 function App() {
-  return (
-    <main>
-      <Header />
-      <Hero />
-      <About />
-    </main>
-  );
+  return <main></main>;
 }
 
 export default App;
